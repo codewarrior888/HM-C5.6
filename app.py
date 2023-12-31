908bbfdc0545fa41a4c7548fd66e5ad9e9481009
codewarrior888 <codewarrior888@users.noreply.github.com>
2023-12-31 22:46:40 +0900

    update

---
+++ app.py
@@ -0,0 +1,41 @@
+import telebot
+from extensions import APIException, Convertor
+from config import TOKEN, currencies
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = ('Добро пожаловать в SimpleExchangeBot!\nВведите команду в следующем формате:\n<базовая валюта> \
+<котируемая валюта> <кол-во>\nУвидеть список всех доступных валют: /values')
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for keys in currencies.keys():
+        text = '\n'.join((text, keys, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    parameters = message.text.split(' ')
+    try:
+        if len(parameters) != 3:
+            raise APIException('Неверное количество параметров!\n\nВведите команду в следующем формате:\
+\n<базовая валюта> <котируемая валюта> <кол-во>')
+
+        answer = Convertor.get_price(*parameters)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка в команде:\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Неизвестная ошибка:\n{e}')
+    else:
+        bot.reply_to(message, answer)
+
+
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '6860485433:AAFPSf8rfqrCcWEvm_E_6OT2BLX5kK-CwTI'
+
+currencies = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
+
